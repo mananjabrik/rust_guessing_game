e381e8b11e634d28af6c748ad23b6db95723c4d3
mananjabrik <manna.jabrik@gmail.com>
2022-06-26 20:13:26 +0700

    learn tuples

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@
 // mod learn_rush;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+mod tuples;
 
 fn main() {
     // learn_rush::test();
     // guessing_game::run();
     // types::run();
-    strings::run();
+    // strings::run();
+    tuples::run();
 }
 // mutable is used to change the value of variable
 // immutable is used to read the value of variable
---
+++ src/tuples.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    let person = ("John", "Doe", 42);
+    println!(
+        "first name {}, lastname {}, age {}",
+        person.0, person.1, person.2
+    );
+}
