307cf1dbbf0f28c907f293095b65543decb30042
mananjabrik <manna.jabrik@gmail.com>
2022-07-02 06:22:39 +0700

    learn while loop, and for loop

--- src/loops.rs
+++ src/loops.rs
@@ -2,11 +2,30 @@ pub fn run() {
     let mut count = 0;
 
     // infinite loop
-    loop {
-        count += 1;
-        println!("number: {}", count);
-        if count == 20 {
-            break;
-        }
+    // loop {
+    //     count += 1;
+    //     println!("number: {}", count);
+    //     if count == 20 {
+    //         break;
+    //     }
+    // }
+    // while loop (Fizz buzz)
+
+    // while count <= 100 {
+    //     if count % 15 == 0 {
+    //         println!("fizzbuzz");
+    //     } else if count % 3 == 0 {
+    //         println!("fizz");
+    //     } else if count % 5 == 0 {
+    //         println!("buzz");
+    //     } else {
+    //         println!("{}", count);
+    //     }
+    //     count += 1;
+    // }
+
+    // for range loop
+    for x in 0..10 {
+        println!("apa cong  {}", x);
     }
 }
