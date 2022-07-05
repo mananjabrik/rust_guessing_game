3b0cc2993a236f5963c71e88aa3bdd104d4d24fc
mananjabrik <manna.jabrik@gmail.com>
2022-07-05 13:22:46 +0700

    learn struct

--- src/structs.rs
+++ src/structs.rs
@@ -7,7 +7,33 @@
 
 // Tuple struct
 
-struct Color(u8, u8, u8);
+// struct Color(u8, u8, u8);
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    fn new(first: &str, las: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: las.to_string(),
+        }
+    }
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+
+    fn set_last_name(&mut self, last: &str) {
+        self.last_name = last.to_string();
+    }
+
+    // Name to topple
+    fn to_tuple(&self) -> (String, String) {
+        (self.first_name.clone(), self.last_name.clone())
+    }
+}
 
 pub fn run() {
     // let mut c: Color = Color {
@@ -20,8 +46,15 @@ pub fn run() {
 
     // println!("color red: {} green: {} blue: {}", c.red, c.green, c.blue);
 
-    let mut c = Color(255, 10, 10);
-    c.1 = 20;
+    // let mut c = Color(255, 10, 10);
+    // c.1 = 20;
 
-    println!("color red: {} green: {} blue: {}", c.0, c.1, c.2);
+    // println!("color red: {} green: {} blue: {}", c.0, c.1, c.2);
+
+    let mut p = Person::new("John", "Jabrik");
+
+    p.set_last_name("Smith");
+
+    println!("{}", p.full_name());
+    println!("{:?}", p.to_tuple());
 }
